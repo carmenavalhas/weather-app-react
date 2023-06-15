588a288bfa00c80598988301d9cd53d26ff8ff40
Carmen <38879535+carmenavalhas@users.noreply.github.com>
2023-06-15 17:02:57 +0100

    Added GitHub Repo URL

--- src/App.js
+++ src/App.js
@@ -3,6 +3,19 @@ import React from 'react';
 import './App.css';
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+  <div className="App">
+    <h1>
+    Weather App
+    </h1>
+    <footer>
+      This project was coded by Carmen Navalhas and it is{" "} 
+    <a 
+    target="_blank" rel="noreferrer">
+   open-sourced on Github
+    </a>
+    </footer>
+  </div>
+);
 }
 

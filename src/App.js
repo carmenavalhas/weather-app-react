e521121dc1e6d40f4e2a7cf5510ee371251f5a6c
Carmen <38879535+carmenavalhas@users.noreply.github.com>
2023-06-15 17:16:13 +0100

    Updated personal link

--- src/App.js
+++ src/App.js
@@ -10,10 +10,10 @@ export default function App() {
     </h1>
     <footer>
       This project was coded by Carmen Navalhas and it is{" "} 
-    <a 
+      <a href="https://github.com/carmenavalhas/weather-app-react"
     target="_blank" rel="noreferrer">
    open-sourced on Github
-    </a>
+    </a>"
     </footer>
   </div>
 );

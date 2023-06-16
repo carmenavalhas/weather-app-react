067f3c8a3c8741cf640cf22ab0f8fb040e53852c
Carmen <38879535+carmenavalhas@users.noreply.github.com>
2023-06-16 14:09:35 +0100

    Added API integration

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
   <div className="App">
     <div className="container">
-      <Weather />
+     <Weather defaultCity="New York" />
     <footer>
       This project was coded by Carmen Navalhas and it is{" "} 
       <a href="https://github.com/carmenavalhas/weather-app-react"
--- src/Weather.js
+++ src/Weather.js
@@ -1,8 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
-  return (
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
+  
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      coordinates: response.data.coord,
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      date: new Date(response.data.dt * 1000),
+      description: response.data.weather[0].description,
+      icon: response.data.weather[0].icon,
+      wind: response.data.wind.speed,
+      city: response.data.name,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
+  function search() {
+    const apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+  
+ if (weatherData.ready) {
+    return (
     <div className="weather">
       <form>
         <div className="row">
@@ -14,31 +48,17 @@ export default function Weather() {
         autoFocus="on"/>
         </div>
         <div className="col-3">
-        <input type="submit" value="Search" className="btn btn-primary w-100"/>
+        <input type="submit" 
+        value="Search" 
+        className="btn btn-primary w-100"/>
       </div>
       </div>
       </form>
-      <h1>New York</h1>
-      <ul>
-        <li>Wednesday 07:00</li>
-        <li>Mostly Cloudy</li>
-      </ul>
-<div className="row mt-3">
-  <div className="col-6">
-<img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" 
-      alt="Mostly Cloudy"/>
-      <span className="temperature">6</span>
-      <span className="unit">ºC</span>
-  </div>
-  <div className="col-6">
-    <ul>
-      <li>Precipitation: 15%</li>
-      <li>Humidity: 72%</li>
-      <li>Wind: 13 km/h</li>
-    </ul>
-  </div>
-</div>
-    </div>
-  )
+      </div>
+    );
 
+     } else {
+    search();
+    return "Loading...";
+  }
 }

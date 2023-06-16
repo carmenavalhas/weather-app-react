c0f2797143e773382479cee5a0d8b783766ccf22
Carmen <38879535+carmenavalhas@users.noreply.github.com>
2023-06-16 19:01:54 +0100

    Added real feel and rounded values

--- src/Weather.js
+++ src/Weather.js
@@ -13,6 +13,7 @@ export default function Weather(props) {
       coordinates: response.data.coord,
       temperature: response.data.main.temp,
       humidity: response.data.main.humidity,
+      feelslike: response.data.main.feels_like,
       date: new Date(response.data.dt * 1000),
       description: response.data.weather[0].description,
       icon: response.data.weather[0].icon,

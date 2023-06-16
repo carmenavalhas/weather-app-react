f948a62afdda7b74402a9cba9c21f5f57e13fad4
Carmen <38879535+carmenavalhas@users.noreply.github.com>
2023-06-16 13:17:19 +0100

    Added style

--- src/Weather.js
+++ src/Weather.js
@@ -23,11 +23,12 @@ export default function Weather() {
         <li>Wednesday 07:00</li>
         <li>Mostly Cloudy</li>
       </ul>
-<div className="row">
+<div className="row mt-3">
   <div className="col-6">
 <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" 
       alt="Mostly Cloudy"/>
-      6ºC
+      <span className="temperature">6</span>
+      <span className="unit">ºC</span>
   </div>
   <div className="col-6">
     <ul>

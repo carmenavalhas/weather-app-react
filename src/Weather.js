2dc236aa9c38bf24ecfa54397324237e66cc6cec
Carmen <38879535+carmenavalhas@users.noreply.github.com>
2023-06-17 16:19:50 +0100

    Added weather forecast for 5 days

--- src/Weather.js
+++ src/Weather.js
@@ -7,7 +7,7 @@ import "./Weather.css";
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
   const [city, setCity] = useState(props.defaultCity);
-  
+
   function handleResponse(response) {
     setWeatherData({
       ready: true,
@@ -61,7 +61,7 @@ export default function Weather(props) {
       </div>
        </form>
         <WeatherInfo data={weatherData} />
-        <WeatherForecast />
+        <WeatherForecast coordinates={weatherData.coordinates} />
       </div>
     );
   } else {
